1cfd7200623068ca9b1955ff3321d05709c009b0
Artur Baranski <artur.baranski@asseco.pl>
2020-01-27 11:52:47 +0100

    Dodaj pliki projektów.

---
+++ UsersRepo_MVC/Controllers/UsersController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using UsersRepo_MVC.Models;
+
+namespace UsersRepo_MVC.Controllers
+{
+    public class UsersController : Controller
+    {
+        private UsersRepo_MVCEntities db = new UsersRepo_MVCEntities();
+
+        // GET: Users
+        public ActionResult Index()
+        {
+            return View(db.tblUsers.ToList());
+        }
+
+        // GET: Users/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tblUsers tblUsers = db.tblUsers.Find(id);
+            if (tblUsers == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tblUsers);
+        }
+
+        // GET: Users/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Users/Create
+        // Aby zapewnić ochronę przed atakami polegającymi na przesyłaniu dodatkowych danych, włącz określone właściwości, z którymi chcesz utworzyć powiązania.
+        // Aby uzyskać więcej szczegółów, zobacz https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,name,email,address")] tblUsers tblUsers)
+        {
+            if (ModelState.IsValid)
+            {
+                db.tblUsers.Add(tblUsers);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(tblUsers);
+        }
+
+        // GET: Users/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tblUsers tblUsers = db.tblUsers.Find(id);
+            if (tblUsers == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tblUsers);
+        }
+
+        // POST: Users/Edit/5
+        // Aby zapewnić ochronę przed atakami polegającymi na przesyłaniu dodatkowych danych, włącz określone właściwości, z którymi chcesz utworzyć powiązania.
+        // Aby uzyskać więcej szczegółów, zobacz https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,name,email,address")] tblUsers tblUsers)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tblUsers).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(tblUsers);
+        }
+
+        // GET: Users/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tblUsers tblUsers = db.tblUsers.Find(id);
+            if (tblUsers == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tblUsers);
+        }
+
+        // POST: Users/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            tblUsers tblUsers = db.tblUsers.Find(id);
+            db.tblUsers.Remove(tblUsers);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ UsersRepo_MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="UsersRepo_MVC.MvcApplication" Language="C#" %>
